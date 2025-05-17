40cfad7b871c8092e4d81bf57376d655057b5adc
Mohit Kumar <mohittjee@gmail.com>
2025-05-17 18:59:45 +0530

    assets add/repositioned

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,23 @@ import App from '../components/App';
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-black">
-      <div className="demo-1__gallery">
-        {[...Array(12)].map((_, i) => (
-          <figure key={i} className={`demo-1__gallery__figure figure-${i + 1}`}>
-            <img
-              className="demo-1__gallery__image"
-              src={`/images/demo-1/${i + 1}.jpg`}
-              alt={`Gallery image ${i + 1}`}
-            />
-          </figure>
-        ))}
+    <div className="h-screen overflow-y-hidden bg-black">
+      <div className="flex flex-col items-center space-y-10 demo-1__gallery opacity-0 p-4">
+        {[...Array(12)].map((_, index) => {
+          const imgIndex = index + 1;
+          return (
+            <figure
+              key={imgIndex}
+              className="w-full max-w-[800px] demo-1__gallery__figure"
+            >
+              <img
+                className="w-full h-auto object-cover demo-1__gallery__image rounded-lg"
+                src={`/images/demo-1/${imgIndex}.jpg`}
+                alt={`Gallery image ${imgIndex}`}
+              />
+            </figure>
+          );
+        })}
       </div>
       <App />
     </div>
